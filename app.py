0c4542c648693a1e7b8c7ac2fcd1098f8b38edb3
E-DOLLZ <eeloh.2303903@stu.cu.edu.ng>
2026-01-22 04:36:31 +0100

    Updated app styling, HTML, and model package

--- app.py
+++ app.py
@@ -54,4 +54,4 @@ def index():
                            feature_bounds=feature_bounds, prediction=prediction, error=error)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
